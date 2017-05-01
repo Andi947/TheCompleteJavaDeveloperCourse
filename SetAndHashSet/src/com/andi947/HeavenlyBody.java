512a165c24a9648b64c99f9b564d5cae84ef473a
Andrea Goldsworthy <andi.goldsworthy@gmail.com>
2017-05-01 20:25:38 +0200

    Added Intro to Sets and HashSets - HeavenlyBody class, addition of planets set, planet moons set and a moons set

---
+++ SetAndHashSet/src/com/andi947/HeavenlyBody.java
@@ -0,0 +1,35 @@
+package com.andi947;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Andrea on 2017/05/01.
+ */
+public final class HeavenlyBody {
+    private final String name;
+    private final double orbitalPeriod;
+    private final Set<HeavenlyBody> satellites;
+
+    public HeavenlyBody(String name, double orbitalPeriod) {
+        this.name = name;
+        this.orbitalPeriod = orbitalPeriod;
+        this.satellites = new HashSet<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getOrbitalPeriod() {
+        return orbitalPeriod;
+    }
+
+    public boolean addMoon(HeavenlyBody moon){
+        return this.satellites.add(moon);
+    }
+
+    public Set<HeavenlyBody> getSatellites() {
+        return new HashSet<>(this.satellites);
+    }
+}
